018d3e863e5cc2d85fbd56ef0da68f52430957d7
“Yulia” <Yulyiamikhailova@gmail.com>
2022-05-11 14:02:21 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Мобильный кошелек");
+
+        int amount = 100;
+        int add = 1100;
+        int total = amount + add;
+        int bonus = add / 100;
+        if (add > 100) {
+            add = 1000;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Бонус: " + bonus);
+        System.out.println("Итоговый счет: " + (total + bonus) + " руб");
+    }
+}
